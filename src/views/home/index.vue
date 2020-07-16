d193989d55587d15068577cbf3ffe3f56f6a7a50
liwei1dao <2211068034@qq.com>
2020-07-17 00:15:37 +0800

    上传布局代码

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,38 +1,42 @@
 <template>
-  <div class="home-container">
-    <div v-loading="loading">
-      <div v-if="!loading">
-        <el-row class="baseitem">
-          <el-col class="baseitem-l"
-                  :span="1">
-            集<i style="padding: 14px;"></i>群:
-          </el-col>
-          <el-col class="baseitem-r"
-                  :span="23">
-            {{baseinfo.tag}}
-          </el-col>
-        </el-row>
-        <el-row>
-          <el-col class="baseitem-l"
-                  :span="1">
-            负<i style="padding: 3px;"></i>责<i style="padding: 3px;"></i>人:
-          </el-col>
-          <el-col class="baseitem-r"
-                  :span="23">
-            李伟
-          </el-col>
-        </el-row>
-        <el-divider></el-divider>
-      </div>
+  <div class="home-container"
+       v-loading="loading">
+    <div v-if="!loading">
+      <h1>{{baseinfo.tag}}</h1>
+      <p>文章，满族，陕西西安人，中国大陆男演员、导演。2006年毕业于中央戏剧学院表演系。曾获大众电影百花奖最佳男演员，金鸡奖最佳导演处女作奖，金鹰奖最具人气男演员等奖项。2018年文章入选《中国电视剧60年大系·人物卷》。2019年7月28日，文章宣布和马伊琍离婚，两人育有两女</p>
+      <el-divider></el-divider>
+      <el-row class="serverlist"
+              :gutter="24">
+        <el-col :xs="8"
+                :sm="6"
+                :md="4"
+                :lg="3">
+          <ServerItem />
+        </el-col>
+        <el-col :xs="8"
+                :sm="6"
+                :md="4"
+                :lg="3">
+          <ServerItem />
+        </el-col>
+        <el-col :xs="8"
+                :sm="6"
+                :md="4"
+                :lg="3">
+          <ServerItem />
+        </el-col>
+      </el-row>
     </div>
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
+import ServerItem from './components/serveritem'
 export default {
   name: 'Home',
   components: {
+    ServerItem,
   },
   data () {
     return {
@@ -66,14 +70,8 @@ export default {
 <style lang="scss" scoped>
 .home-container {
   padding: 30px;
-  .baseitem {
-    margin-bottom: 20px;
-    .baseitem-l {
-      font-weight: normal;
-    }
-    .baseitem-r {
-      // margin-right: 30px;
-      font-weight: bolder;
+  .serverlist {
+    .el-col {
     }
   }
 }
